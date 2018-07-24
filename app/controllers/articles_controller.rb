4069194f3d4e9373670b2690a3ca3b19830a0d2e
Christine McCreary <cmccre@uw.edu>
2018-07-24 15:37:17 -0700

    edit/destroy...

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,17 @@
 class ArticlesController < ApplicationController
 
-	before_action :find_article, only: [:show]
+	before_action :find_article, only: [:show, :edit, :update, :destroy]
 
 	# From Devise
 	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
-		@articles = Article.all.order('created_at DESC')
+		if params[:category].blank?
+			@articles = Article.all.order('created_at DESC')
+		else
+			@category_id = Category.find_by(name: params[:category]).id
+			@articles = Article.where(category_id: @category_id).order('created_at DESC')
+		end
 	end
 
 	def new
@@ -20,7 +25,22 @@ class ArticlesController < ApplicationController
 		else
 			render 'new'
 		end
+	end
 
+	def edit
+	end
+
+	def update
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article.destroy
+		redirect_to root_path
 	end
 
 	def show
@@ -29,7 +49,7 @@ class ArticlesController < ApplicationController
 	private
 
 	def article_params
-		params.require(:article).permit(:title, :content)
+		params.require(:article).permit(:title, :content, :category_id)
 	end
 
 	def find_article
