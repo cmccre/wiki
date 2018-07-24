822f7218c54540922e51d5e81d692adcb72de1eb
Christine McCreary <cmccre@uw.edu>
2018-07-24 12:38:36 -0700

    Associated User and Article

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_24_191753) do
+ActiveRecord::Schema.define(version: 2018_07_24_192527) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
