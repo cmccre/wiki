cfb438a259522057046147207eee1e00bad35420
Christine McCreary <cmccre@uw.edu>
2018-07-24 12:45:41 -0700

    Added Authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,9 @@ class ArticlesController < ApplicationController
 
 	before_action :find_article, only: [:show]
 
+	# From Devise
+	before_action :authenticate_user!, except: [:index, :show]
+
 	def index
 		@articles = Article.all.order('created_at DESC')
 	end
