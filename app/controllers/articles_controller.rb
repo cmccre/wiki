1624fb8f508fe398c4c7e3eaf183bbe5c1a6ff6f
Christine McCreary <cmccre@uw.edu>
2018-07-24 11:52:29 -0700

    installed simple_form and initial article MVC

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,34 @@
+class ArticlesController < ApplicationController
+
+	before_action :find_article, only: [:show]
+
+	def index
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
+
+	end
+
+	def show
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :content)
+	end
+
+	def find_article
+		@article = Article.find(params[:id])
+	end
+end
