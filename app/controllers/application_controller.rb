af00296b0dc37d191267d9e8d00fd0ba20339419
Christine McCreary <cmccre@uw.edu>
2018-08-05 13:17:47 -0700

    added user editing/deletion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+  	def configure_permitted_parameters
+    	update_attrs = [:password, :password_confirmation, :current_password]
+    	devise_parameter_sanitizer.permit :account_update, keys: update_attrs
+  	end
+
+
 	def authorize_admin
     	redirect_to root_path, alert: 'Access Denied' unless current_user.is_admin
-  end
+  	end
 end
