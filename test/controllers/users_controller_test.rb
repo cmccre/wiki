9fe3cded77f9e777a9a0a5de07c031d16b6ff51b
Christine McCreary <cmccre@uw.edu>
2018-09-08 16:02:54 -0700

    users controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -9,6 +9,15 @@ class UsersControllerTest < ActionController::TestCase
 	  	assert_equal "index", @controller.action_name
 	end
 
+	test "should not get user index without signing in as admin" do
+		user = create(:user)
+		sign_in user
+	  	get :index
+	  	assert_redirected_to root_path
+	  	assert_equal "Access Denied", flash[:alert]
+	  	assert_equal "index", @controller.action_name
+	end
+
 	test "should get user index with one admin user" do
 		user = create(:user, :admin)
 		sign_in user
@@ -45,6 +54,15 @@ class UsersControllerTest < ActionController::TestCase
 	  	assert_equal "show", @controller.action_name
 	end
 
+	test "should not get user show without signing in as admin" do
+		user = create(:user)
+		sign_in user
+	  	get :show, params: { id: user.id }
+	  	assert_redirected_to root_path
+	  	assert_equal "Access Denied", flash[:alert]
+	  	assert_equal "show", @controller.action_name
+	end
+
 	test "should get user show with one admin user" do
 		user = create(:user, :admin)
 		sign_in user
@@ -69,5 +87,47 @@ class UsersControllerTest < ActionController::TestCase
 	  	get :show, params: { id: user1.id }
 	  	assert_response :success
 	  	assert_equal "show", @controller.action_name
+	end
+
+	test "should not ban editing user without signing in" do
+		user1 = create(:user)
+		user = create(:user, :admin)
+		assert_equal user1.is_active, true
+		post :toggle_is_active, params: { id: user1.id }
+		assert_redirected_to new_user_session_path
+	  	assert_equal "toggle_is_active", @controller.action_name
+	end
+
+	test "should not ban editing user without signing in as admin" do
+		user1 = create(:user)
+		user = create(:user, :admin)
+		sign_in user1
+		assert_equal user.is_active, true
+		post :toggle_is_active, params: { id: user.id }
+		assert_redirected_to root_path
+	  	assert_equal "Access Denied", flash[:alert]
+	  	assert_equal "toggle_is_active", @controller.action_name
+	end
+
+	test "should ban editing user" do
+		user1 = create(:user)
+		user = create(:user, :admin)
+		sign_in user
+		assert_equal user1.is_active, true
+		put :toggle_is_active, params: { id: user1.id }
+	  	assert_equal assigns[:user].is_active, false
+	  	assert_redirected_to users_path
+	  	assert_equal "toggle_is_active", @controller.action_name
+	end
+
+	test "should unban banned user" do
+		user1 = create(:user, :banned)
+		user = create(:user, :admin)
+		sign_in user
+		assert_equal user1.is_active, false
+		put :toggle_is_active, params: { id: user1.id }
+	  	assert_equal assigns[:user].is_active, true
+	  	assert_redirected_to users_path
+	  	assert_equal "toggle_is_active", @controller.action_name
 	end
 end
